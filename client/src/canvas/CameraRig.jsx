2b310994019e95176a8323f1a402aa3ee15b1a88
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2023-05-12 18:30:39 +0900

    fixed mobile view, finished customizer and tabs

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -18,10 +18,9 @@ const CameraRig = ({ children }) => {
 		if (snap.intro) {
 			if (isBreakpoint) targetPosition = [0, 0, 2];
 			if (isMobile) targetPosition = [0, 0.2, 2.5];
-			else {
-				if (isMobile) targetPosition = [0, 0, 2.5];
-				else targetPosition = [0, 0, 2];
-			}
+		} else {
+			if (isMobile) targetPosition = [0, 0.2, 2.5];
+			else targetPosition = [0, 0, 2];
 		}
 
 		//set model camera position
