ba3c80b0bb9cdb89779bcc609cc4e03153dfa2c5
shokhrukh-rt <mr.shohruh.1003@gmail.com>
2023-05-15 16:07:33 +0900

    finished ai image generation

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -43,13 +43,46 @@ const Customizer = () => {
 					/>
 				);
 			case "aipicker":
-				return <AIPicker />;
+				return (
+					<AIPicker
+						prompt={prompt}
+						setPrompt={setPrompt}
+						generatingImg={generatingImg}
+						handleSubmit={handleSubmit}
+					/>
+				);
 
 			default:
 				return null;
 		}
 	};
 
+	const handleSubmit = async (type) => {
+		if (!prompt) return alert("Please enter a prompt");
+
+		try {
+			setGeneratingImg(true);
+
+			const response = await fetch("http://localhost:8080/api/v1/dalle", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					prompt,
+				}),
+			});
+
+			const data = await response.json();
+
+			handleDecals(type, `data:image/png;base64,${data.photo}`);
+		} catch (error) {
+			alert(error);
+		} finally {
+			setGeneratingImg(false);
+			setActiveEditorTab("");
+		}
+	};
 	const handleDecals = (type, result) => {
 		const decalType = DecalTypes[type];
 
@@ -66,9 +99,11 @@ const Customizer = () => {
 				break;
 			case "stylishShirt":
 				state.isFulTexture = !activeFilterTab[tabName];
+				break;
 			default:
 				state.isFullTexture = false;
 				state.isLogoTexture = true;
+				break;
 		}
 		setActiveFilterTab((prevState) => {
 			return {
